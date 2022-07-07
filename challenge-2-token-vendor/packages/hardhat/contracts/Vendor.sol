4fac01289101037cec8b4f25f71efafd93183d6a
edddyguo <edddyguo@gmail.com>
2022-07-07 10:24:04 +0800

    +1

--- challenge-2-token-vendor/packages/hardhat/contracts/Vendor.sol
+++ challenge-2-token-vendor/packages/hardhat/contracts/Vendor.sol
@@ -6,18 +6,62 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event SellTokens(address seller, uint256 amountOfTokens, uint256 amountOfETH);
 
-  YourToken public yourToken;
+    uint256 public constant tokensPerEth = 100;
+    YourToken public yourToken;
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  // ToDo: create a payable buyTokens() function:
+    // ToDo: create a payable buyTokens() function:
+    function buyTokens() public payable returns (uint256 tokenAmount){
+        require(msg.value > 0, "Send ETH to buy some tokens");
+        uint256 amountToBuy = msg.value * tokensPerEth;
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+        //must be less than remain amount
+        uint256 vendorBalance = yourToken.balanceOf(address(this));
+        require(vendorBalance >= amountToBuy, "Vendor contract has not enough tokens in its balance");
 
-  // ToDo: create a sellTokens(uint256 _amount) function:
+        (bool sent) = yourToken.transfer(msg.sender, amountToBuy);
+        require(sent, "Failed to transfer token to user");
+
+        emit BuyTokens(msg.sender, msg.value, amountToBuy);
+
+        return amountToBuy;
+    }
+
+    // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function sellTokens(uint256 tokenAmountToSell) public {
+        // Check that the requested amount of tokens to sell is more than 0
+        require(tokenAmountToSell > 0, "Specify an amount of token greater than zero");
+
+        // Check that the user's token balance is enough to do the swap
+        uint256 userBalance = yourToken.balanceOf(msg.sender);
+        require(userBalance >= tokenAmountToSell, "Your balance is lower than the amount of tokens you want to sell");
+
+        // Check that the Vendor's balance is enough to do the swap
+        uint256 amountOfETHToTransfer = tokenAmountToSell / tokensPerEth;
+        uint256 ownerETHBalance = address(this).balance;
+        require(ownerETHBalance >= amountOfETHToTransfer, "Vendor has not enough funds to accept the sell request");
+
+        (bool sent) = yourToken.transferFrom(msg.sender, address(this), tokenAmountToSell);
+        require(sent, "Failed to transfer tokens from user to vendor");
+
+
+        (sent,) = msg.sender.call{value: amountOfETHToTransfer}("");
+        require(sent, "Failed to send ETH to the user");
+        emit SellTokens(msg.sender,tokenAmountToSell, amountOfETHToTransfer);
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 ownerBalance = address(this).balance;
+        require(ownerBalance > 0, "Owner has not balance to withdraw");
+
+        (bool sent,) = msg.sender.call{value: address(this).balance}("");
+        require(sent, "Failed to send user balance back to the owner");
+    }
 
 }
